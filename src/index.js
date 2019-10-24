1c6263da3b8b3b49a038de2bd1420a1fac58802e
Dima <dstepas@justcoded.co>
2019-10-24 19:46:48 +0300

    connect controllers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { BOT_API_KEY } from './constants';
-
-import messageStartController from './controllers/messageStartController';
-import messageCityController from './controllers/messageCityController';
-import selectController from './controllers/selectController';
+import initControllers from './controllers/initControllers';
 
 const bot = new TelegramBot(BOT_API_KEY, { polling: true });
 
-messageStartController(bot);
-
-messageCityController(bot);
-
-selectController(bot);
+initControllers(bot);
